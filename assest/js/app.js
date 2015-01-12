730638d6da7f214b5b766b23d5d6769d6be49fe4
zeldanerd <zeldanerd123@hotmail.com>
2015-01-11 20:49:11 -0500

    Remap
    Changed the code and layout

---
+++ assest/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+ 
+        //Calculate the height of <nav>
+        //Use outerHeight() instead of height() if have padding
+	var navHeight = $('nav').height();
+ 
+	//when scroll
+	$(window).scroll(function() {
+ 
+	        //if scrolled down more than the header’s height
+		if ($(window).scrollTop() > navHeight) {
+ 
+	        // if yes, add “fixed” class to the <nav>
+	        // add padding top to the #content 
+               // (value is same as the height of the nav)
+			$('nav').addClass('fixed');
+                
+ 
+		} else {
+ 
+	        // when scroll up or less than aboveHeight,
+               // remove the “fixed” class, and the padding-top
+			$('nav').removeClass('fixed');
+		}
+	});
+});
